f5f2221c725ca0efb27b8098ca2f6ad0ccbd653f
ayushsonani <145139790+ayushsonani@users.noreply.github.com>
2023-12-17 09:16:14 +0530

    update

--- lib/calculator_bloc_controller/calculator_bloc_controller_bloc.dart
+++ lib/calculator_bloc_controller/calculator_bloc_controller_bloc.dart
@@ -9,30 +9,36 @@ class Calculator_bloc_controllerBloc extends Bloc<Calculator_bloc_controllerEven
   Calculator_bloc_controllerBloc() : super(Calculator_bloc_controllerState().init()) {
     on<InitEvent>(_init);
     on<NumberInsert>((event, emit) {
+      String temp = Calculator_bloc_controllerPage.userinsert;
+
 
       if(event.numberstring=="="){
         Parser p = Parser();
-        print("allnumbner := ${Calculator_bloc_controllerPage.userinsert}");
-        Expression exp = p.parse(Calculator_bloc_controllerPage.userinsert);
+        print("allnumbner := ${temp}");
+        Expression exp = p.parse(temp);
         ContextModel ctx = ContextModel();
         double eval = exp.evaluate(EvaluationType.REAL, ctx);
 
         print("evnl := ${eval}");
         emit(TotalNumber(answer: eval.toString()));
         Calculator_bloc_controllerPage.userinsert = "";
+        temp = "";
       }
       else if(event.numberstring=="AC"){
-        Calculator_bloc_controllerPage.userinsert="";
+        Calculator_bloc_controllerPage.userinsert = "";
+        emit(TotalNumber(answer: Calculator_bloc_controllerPage.userinsert));
       }
       else if(event.numberstring=="<="){
+        temp = temp.substring(0,temp.length-1);
         Calculator_bloc_controllerPage.userinsert = Calculator_bloc_controllerPage.userinsert.substring(0,Calculator_bloc_controllerPage.userinsert.length-1);
+        emit(TotalNumber(answer: Calculator_bloc_controllerPage.userinsert));
       }
       else{
+        temp = temp + event.numberstring;
         Calculator_bloc_controllerPage.userinsert =Calculator_bloc_controllerPage.userinsert + event.numberstring  ;
         print("Numbner := ${Calculator_bloc_controllerPage.userinsert}");
         emit(TotalNumber(answer: Calculator_bloc_controllerPage.userinsert));
       }
-
     });
 
   }
